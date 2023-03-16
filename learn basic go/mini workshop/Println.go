702cb5a257261a6fffcb4c1e22bae5fc0f201da5
Nataphan rakvidhyasatra <85008989+NRdeARK@users.noreply.github.com>
2023-03-16 20:44:46 +0700

    Create Println.go

---
+++ learn basic go/mini workshop/Println.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	//workshop: println
+	//output: 
+	//title : avengers : NoEndgame
+	//year : 2-0-1-9
+	//rating : 8.8
+	//type : action
+
+	//todo : write code below
+}
