ba3ad8ee7c601036718a8651086bcd18048bb30a
Nataphan rakvidhyasatra <85008989+NRdeARK@users.noreply.github.com>
2023-03-16 21:01:58 +0700

    Create Printf.go

---
+++ learn basic go/mini workshop solution/Printf.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Workshop: Printf
+
+	// Output:
+	// เรื่อง: Avengers: Endgame
+	// ปี: 2019
+	// เรตติ้ง: 8.4
+	// ประเภท: Sci-Fi
+	// ซุปเปอร์ฮีโร่: true
+	// เรื่องโปรด: ⭐
+
+	// TODO: write code below.
+	key,value := "เรื่อง:", " Avengers: Endgame"
+	fmt.Printf("%s%s\n",key,value)
+
+	key,valueD := "ปี:", 2019
+	fmt.Printf("%s%d\n",key,valueD)
+	
+	key,valueF := "เรตติ้ง:", 8.4
+	fmt.Printf("%s%f\n",key,valueF)
+	
+	key,value = "ประเภท:", " Sci-Fi"
+	fmt.Printf("%s%s\n",key,value)
+	
+	key,valueB := "ซุปเปอร์ฮีโร่:", true
+	fmt.Printf("%s%v\n",key,valueB)
+	
+	key,valueC := "เรื่องโปรด:", " ⭐"
+	fmt.Printf("%s%s\n",key,valueC)
+
+}
