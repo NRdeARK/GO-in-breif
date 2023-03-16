134e04280bbf79b1c71d76d216723fb6ce3b26b2
Nataphan rakvidhyasatra <85008989+NRdeARK@users.noreply.github.com>
2023-03-16 20:44:43 +0700

    Create Println.go

---
+++ learn basic go/mini workshop solution/Println.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//workshop: println
+	//output:
+	//title : avengers : NoEndgame
+	//year : 2-0-1-9
+	//rating : 8.8
+	//type : action
+
+	//todo : write code below
+	title := "title : avengers : NoEndgame"
+	year := "year : 2-0-1-9"
+	rating := "rating : 8.8"
+	typemovie := "type : action"
+	fmt.Println(title+"\n"+year+"\n"+rating+"\n"+typemovie)
+}
