1213fb7a9fa6f6dc514689ca47dc7c434e030f34
Nataphan rakvidhyasatra <85008989+NRdeARK@users.noreply.github.com>
2023-03-16 21:01:56 +0700

    Create Printf.go

---
+++ learn basic go/mini workshop/Printf.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	// Workshop: Printf
+
+	// Output:
+	// เรื่อง: Avengers: Endgame
+	// ปี: 2019
+	// เรตติ้ง: 8.4
+	// ประเภท: Sci-Fi
+	// ซุปเปอร์ฮีโร่: true
+	// เรื่องโปรด: ⭐
+
+	// TODO: write code below.
+
+}
