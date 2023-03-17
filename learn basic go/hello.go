1f2400ba79285a52b418d6793f07da0a73922d6f
Nataphan rakvidhyasatra <85008989+NRdeARK@users.noreply.github.com>
2023-03-17 16:49:48 +0700

    Update hello.go

--- learn basic go/hello.go
+++ learn basic go/hello.go
@@ -2,6 +2,6 @@ package main// every go file need to have this
 
 import "fmt"// import fmt(format)  
 
-func main() {// every thing start at here "main" function
+func main() {// every thing start at here "main" function "{" is fixed to be there
 	fmt.Println("Hello Gopher!!!")
 }
