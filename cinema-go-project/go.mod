841077ecf6a5d1bc25bd70c05687b4193bf44af8
Nataphan rakvidhyasatra <85008989+NRdeARK@users.noreply.github.com>
2023-03-20 23:58:01 +0700

    move project

--- cinema-go-project/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/NRdeARK/cinema-go-project
-
-go 1.20
--- cinema-go-project/ticket/ticket.go
+++
@@ -1,7 +0,0 @@
-package ticket
-
-import "fmt"
-
-func BuyTicket(movie string) {
-	fmt.Printf("I bought tickets to %s\n", movie)
-}
